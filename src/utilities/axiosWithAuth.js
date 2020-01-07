80b75eda01e0f166f552e33ec7873251bd43a868
tim-jeffries <timothyrexjeffries@gmail.com>
2020-01-06 19:50:03 -0800

    Created enrtyActions.js, 90%

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from "axios"
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem("token");
-  const user_id = localStorage.getItem("user_id");
+  const token = localStorage.getItem("token")
+  const user_id = localStorage.getItem("user_id")
   return axios.create({
-    baseURL: "",
+    baseURL: "https://gigapets-winter-2019.herokuapp.com",
     headers: {
       Authorization: token,
       user_id: user_id
     }
-  });
-};
+  })
+}
